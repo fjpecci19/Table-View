eb5e0d89bfb1dcb3ea004286efdefa1bfdeff8d8
Franco Pecci <fapeccisan@gmail.com>
2023-08-21 17:11:35 -0400

    Lógica terminada, falta diseñar mejor

---
+++ Table View/CustomTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.swift
+//  Table View
+//
+//  Created by Franco Pecci on 2023-08-21.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var money: UILabel!
+}
--- Table View/ViewController.swift
+++ Table View/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var table: UITableView!
+    
+    struct transfers {
+        let transaction: String
+        let money: String
+    }
+    
+    let data: [transfers] = [
+        transfers(transaction: "verde", money: "25000"),
+        transfers(transaction: "rojo", money: "-22000"),
+        transfers(transaction: "verde", money: "25000"),
+        transfers(transaction: "rojo", money: "-22000"),
+        transfers(transaction: "verde", money: "25000"),
+        transfers(transaction: "rojo", money: "-22000")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let transfer = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.icon.image = UIImage(named: transfer.transaction)
+        cell.money.text = transfer.money
+        return cell
+    }
+}
